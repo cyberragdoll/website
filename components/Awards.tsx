9037aca398880dd62225b0ed1fb9df345ddfc8d1
cyberragdoll <sevensleen@gmail.com>
2026-02-22 17:36:58 +0100

    Update Awards.tsx

--- components/Awards.tsx
+++ components/Awards.tsx
@@ -89,8 +89,9 @@ const Awards: React.FC = () => {
 }
 
 /* Track */
-.modal-scroll::-webkit-scrollbar-track {
-  background: transparent;
+.modal-scroll::-webkit-scrollbar-thumb {
+  background-color: #cbd5e1; /* slate-300 */
+  border-radius: 999px;
 }
 
 /* Thumb (the draggable bar) */
